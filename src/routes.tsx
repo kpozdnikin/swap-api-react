425763f128f56833d6da119e734ef86569466722
kpozdnikin <kpozdnikin@gmail.com>
2023-12-05 22:03:32 +0300

    [characters-list]: lazy загрузка страниц

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
+import { lazy } from 'react';
 
-import { DetailsPage, MainPage } from './pages';
-
-// const App = lazy(() => import('./App').then((module) => ({ default: module.App })));
+const MainPage = lazy(() => import('./pages').then((module) => ({ default: module.MainPage })));
+const DetailsPage = lazy(() =>
+    import('./pages').then((module) => ({ default: module.DetailsPage })),
+);
 
 export const router = createBrowserRouter([
     {
